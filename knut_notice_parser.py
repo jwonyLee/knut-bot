d9d37e0127a376556af1cd4c311ae401f927c713
jiwon <jiwon.swdev@gmail.com>
2020-01-30 15:51:33 +0900

    기능 추가

--- knut_notice_parser.py
+++ knut_notice_parser.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import telegram
 import secret
+import time
 
 bot = telegram.Bot(token=secret.TELEGRAM_TOKEN)
 chat_id = secret.TELEGRAM_CHAT_ID
@@ -13,53 +14,53 @@ conn = psycopg2.connect(conn_setup)
 cur = conn.cursor()
 
 # get Board title, id
-searchBBS_query = 'select title, bbs_id from knut_bbs;'
-cur.execute(searchBBS_query)
-searchBBS_result = cur.fetchall()
+def getBoardInfo():
+    searchBBS_query = 'select title, bbs_id from knut_bbs;'
+    cur.execute(searchBBS_query)
+    searchBBS_result = cur.fetchall()
+    return searchBBS_result
 
-def latest_notice(bbs_title):
-    latest_query = 'select title from knut_article where bbs_title = \'%s\' LIMIT 1;' % bbs_title
-    cur.execute(latest_query)
+def getArticle(article_title):
+    search_query = 'select count(title) from knut_article where title = \'%s\';' % article_title
+    cur.execute(search_query)
 
-    if cur.fetchall is not None:
-        latest_result = cur.fetchall().pop()[0]
-        return latest_result
+    result = cur.fetchone()[0]
+    if result:
+        return False
     else:
-        return None
+        return True
 
+def getNotice():
+    board = getBoardInfo()
+    for (bbs_title, bbs_id) in board:
+        link = 'https://www.ut.ac.kr/cop/bbs/BBSMSTR_' + bbs_id + '/selectBoardList.do'
 
-for (bbs_title, bbs_id) in searchBBS_result:
-    print(bbs_id + bbs_title)
-    link = 'https://www.ut.ac.kr/cop/bbs/BBSMSTR_' + bbs_id + '/selectBoardList.do'
+        # URL 접근
+        req = requests.get(link)
+        req.encoding = 'utf-8'
 
-    # URL 접근
-    req = requests.get(link)
-    req.encoding = 'utf-8'
+        # 게시판 첫 페이지 게시글 제목, 게시글 고유 번호 크롤링
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        notices = soup.select('td.left > div > div > form > input[type=submit]')
+        nttId = soup.select('td.left > div > div > form > input[type=hidden]:nth-child(2)')
 
-    # 게시글 제목, 게시글 고유 번호 크롤링
-    html = req.text
-    soup = BeautifulSoup(html, 'html.parser')
-    notices = soup.select('td.left > div > div > form > input[type=submit]')
-    nttId = soup.select('td.left > div > div > form > input[type=hidden]:nth-child(2)')
+        for title, nid in zip(notices, nttId):
+            article_title = title.get('value')
+            article_id = nid.get('value')
+            link = 'https://www.ut.ac.kr/cop/bbs/BBSMSTR_' + bbs_id + '/selectBoardArticle.do?nttId=' + article_id
 
-    for title, nid in zip(notices, nttId):
-        article_title = title.get('value')
-        article_id = nid.get('value')
-        link = 'https://www.ut.ac.kr/cop/bbs/BBSMSTR_' + bbs_id + '/selectBoardArticle.do?nttId=' + article_id
+            # 데이터베이스에 중복된 게시글이 없다면 데이터베이스에 저장 후 텔레그램 채널로 메시지 전송
+            if getArticle(article_title):
+                try:
+                    insert_query = 'insert into knut_article values (default , %s,%s,%s) ON CONFLICT(title) DO NOTHING;'
+                    cur.execute(insert_query, (bbs_title, article_title, link))
+                    conn.commit()
+                    message = '[' + bbs_title + '] ' + article_title + '\n' + link
+                    bot.sendMessage(chat_id=chat_id, text=message)
+                except Exception as e:
+                    print(e)
 
-        # 가장 최근의 게시글을 가져옴
-        latest_title = latest_notice(bbs_title)
-        
-        try:
-            # 가장 최근 게시글과 같지 않다면 (= 새 글이라면)
-            # 데이터베이스에 해당 게시글을 저장, 텔레그램 채널로 메시지 전송
-            if title != latest_title:
-                insert_query = 'insert into knut_article values (default , %s,%s,%s) ON CONFLICT(title) DO NOTHING;'
-                cur.execute(insert_query, (bbs_title, article_title, link))
-                conn.commit()
-                message = '[' + bbs_title + '] ' + article_title + '\n' + link
-                bot.sendMessage(chat_id=chat_id, text=message)
-            else:
-                continue
-        except Exception as e:
-            print(e)
+if __name__ == "__main__":
+    start_time = time.time()
+    getNotice()
